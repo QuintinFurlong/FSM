24986b6203a828de38ee84ce030cedde2316b118
Quintin Furlong <C00218453@itcarlow.ie>
2017-11-09 11:25:37 +0000

    small changes

--- Finate State Machine/Finate State Machine/Game.cpp
+++ Finate State Machine/Finate State Machine/Game.cpp
@@ -82,12 +82,6 @@ void Game::processEvents()
 /// <param name="t_deltaTime">time interval per frame</param>
 void Game::update(sf::Time t_deltaTime)
 {
-	/*std::cout << "countdown:\n";
-	for (int i = 10; i>0; --i) {
-		std::cout << i << std::endl;
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-	}
-	std::cout << "Lift off!\n";*/
 }
 
 /// <summary>
